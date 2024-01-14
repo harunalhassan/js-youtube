482e34c3c22a080d7b4df0e5a465ba8d8bd51ecc
harunalhassan <128882187+harunalhassan@users.noreply.github.com>
2024-01-14 07:07:28 +0000

    constnt and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId= 12345
+let accountemail="harun@gmail.com"
+var accountCIty="Ghy"
+accountPassword=1234
+let accountState
+
+console.table([accountId,accountemail,accountCIty,accountPassword,accountState])
