23b6b87dd0634f43590c7bea49d9946b04006971
harunalhassan <128882187+harunalhassan@users.noreply.github.com>
2024-01-26 05:49:07 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,18 @@
+const Myname= "Harun"
+const Myid= 21
+
+//modern way to string interpolation by using backticks ``
+console.log(`hello i am ${Myname} and my Id is ${Myid}`)
+
+const mSentence= new String('Hello');
+console.log(mSentence)
+console.log(mSentence.length)
+console.log(mSentence.toUpperCase());
+console.log(mSentence.toLowerCase());
+console.log(mSentence.charAt(4));
+console.log(mSentence.indexOf('l'));
+
+const GameName="     Valorant    "
+console.log(GameName)
+console.log(GameName.trim())
+console.log(GameName.trimEnd())
